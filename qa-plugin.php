f9728c4eefeb67a1d02d61ab9517409c433ac846
EC2 Default User <ec2-user@ip-172-31-29-147.ap-northeast-1.compute.internal>
2016-05-18 07:45:07 +0900

    refactor

--- qa-plugin.php
+++ qa-plugin.php
@@ -2,7 +2,7 @@
 
 /*
 	Plugin Name: POPUP AD
-	Plugin URI: 
+	Plugin URI:
 	Plugin Description: popup add
 	Plugin Version: 0.1
 	Plugin Date: 2015-09-11
@@ -10,13 +10,12 @@
 	Plugin Author URI:https://github.com/yshiga
 	Plugin License: GPLv2
 	Plugin Minimum Question2Answer Version: 1.7
-	Plugin Update Check URI: 
+	Plugin Update Check URI:
 */
-
 if (!defined('QA_VERSION')) { // don't allow this page to be requested directly from browser
 	header('Location: ../../');
 	exit;
 }
 
-qa_register_plugin_layer('qa-popup-ad-layer.php', 'POPUP ADD');	
+qa_register_plugin_layer('qa-popup-ad-layer.php', 'POPUP ADD');
 qa_register_plugin_module('module', 'qa-popup-ad-admin.php', 'qa_popup_ad_admin', 'popup_ad');
--- qa-popup-ad-layer.php
+++ qa-popup-ad-layer.php
@@ -3,33 +3,43 @@ class qa_html_theme_layer extends qa_html_theme_base {
 
 	var $plugin_url;
 
+
 	// needed to get the plugin url
 	function qa_html_theme_layer($template, $content, $rooturl, $request)
 	{
 		qa_html_theme_base::qa_html_theme_base($template, $content, $rooturl, $request);
 		if(!$this->shouldShowPopup()) {
 			return;
-		} 
+		}
 		global $qa_layers;
 		$this->plugin_url = $qa_layers['POPUP ADD']['urltoroot'];
 	}
 
 	function head_script() {// insert Javascript into the <head>
-  		qa_html_theme_base::head_script();
+  	qa_html_theme_base::head_script();
 		if(!$this->shouldShowPopup()) {
 			return;
-		} 
+		}
 
-		$this->output('<script type="text/javascript" src="'. qa_opt('site_url') . $this->plugin_url.'/vender/popup.js"></script>');
-		$this->output("<script>
-		$(window).load(function () {
-			var popup = new $.Popup({
-				width : 320,
-				height: 300
-			});
-			popup.open('" . qa_opt('qa-popup-ad-html') . "', 'html');
-		});
-		</script>");
+		$library_src = qa_opt('site_url') . $this->plugin_url . '/vender/popup.js';
+		$this->output('<script type="text/javascript" src="'. $library_src . '"></script>');
+
+		$html = qa_opt('qa-popup-ad-html');
+		$html = preg_replace(array('/\r\n/','/\r/','/\n/'), '', $html); // remove line break
+
+		$js = <<<"EOT"
+<script>
+$(window).load(function () {
+	var popup = new $.Popup({
+		width : 320,
+		height: 300
+	});
+	popup.open('$html', 'html');
+});
+</script>
+EOT;
+
+		$this->output($js);
 	}
 
 	function head_css() {
@@ -38,16 +48,59 @@ class qa_html_theme_layer extends qa_html_theme_base {
 
 		if(!$this->shouldShowPopup()) {
 			return;
-		} 
+		}
 
-		$this->output('<style>
-/*------------------------------- POPUP.CSS -------------------------------*/ 
-.popup_back { height: 100%; left: 0; 
-		position: fixed; 
-		top: 0; width: 100%; z-index: 100; 
-		} 
-.popup_cont { position: fixed; z-index: 102; } .preloader { z-index: 101; } /*--------------------- EDIT BELOW */ .popup_close { color: #888; cursor: pointer; position: absolute; padding: 5px; right: 5px; top: 0; } .popup_close:hover { color: #111; } .popup_back { cursor: pointer; background-color: #222; } div.popup { background: #fff; padding: 25px; box-shadow: 0 3px 10px #222; } .preloader { left: 50%; margin: -10px 0 0 -25px; position: fixed; top: 50%;} </style>');
+		$css = <<<"EOT"
+<style>
+/*------------------------------- POPUP.CSS -------------------------------*/
+.popup_back {
+	height: 100%; left: 0;
+	position: fixed;
+	top: 0; width: 100%; z-index: 100;
+}
+.popup_cont {
+	position: fixed;
+	z-index: 102;
+}
+.preloader {
+	z-index: 101;
+}
 
+/*--------------------- EDIT BELOW */
+.popup_close {
+	color: #888;
+	cursor: pointer;
+	position: absolute;
+	padding: 5px;
+	right: 5px;
+	top: 0;
+}
+
+.popup_close:hover {
+	color: #111;
+}
+
+.popup_back {
+	cursor: pointer;
+	background-color: #222;
+}
+
+div.popup {
+	background: #fff;
+	padding: 25px;
+	box-shadow: 0 3px 10px #222;
+}
+
+.preloader {
+	left: 50%;
+	margin: -10px 0 0 -25px;
+	position: fixed;
+	top: 50%;
+}
+</style>
+EOT;
+
+		$this->output($css);
 	}
 
 	function head_custom() {
@@ -56,12 +109,12 @@ class qa_html_theme_layer extends qa_html_theme_base {
 
 	private function shouldShowPopup(){
 
-                $blackList = array('/ask', '/login', '/reset');
-                $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+		$blackList = array('/ask', '/login', '/reset');
+  	$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-                if(in_array($path, $blackList)) {
+  	if(in_array($path, $blackList)) {
 			return false;
-                }
+  	}
 
 		if(qa_is_logged_in()) {
 			return false;
@@ -69,7 +122,7 @@ class qa_html_theme_layer extends qa_html_theme_base {
 
 		if($this->isJustLand()) {
 			return true;
-		} 
+		}
 
 		return false;
 	}
