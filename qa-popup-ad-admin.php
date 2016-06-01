aa1385bbbe957a2772824e44cd5b1001255363dd
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-06-01 15:45:29 +0900

    [add]plugin admin page options added

--- qa-popup-ad-admin.php
+++ qa-popup-ad-admin.php
@@ -2,53 +2,138 @@
 
 class qa_popup_ad_admin
 {
-    public function init_queries($tableslc)
-    {
-        return;
-    }
-    public function option_default($option)
-    {
-        switch ($option) {
-            case 'qa-popup-ad-html':
-                return '';
-            default:
-                return;
-        }
-    }
+	public function init_queries($tableslc)
+	{
+		return;
+	}
+	public function option_default($option)
+	{
+		switch ($option) {
+			case 'qa-popup-ad-html':
+				return '';
+			default:
+				return;
+		}
+	}
 
-    public function allow_template($template)
-    {
-        return $template != 'admin';
-    }
+	public function allow_template($template)
+	{
+		return $template != 'admin';
+	}
 
-    public function admin_form(&$qa_content)
-    {
-        // process the admin form if admin hit Save-Changes-button
-        $ok = null;
-        if (qa_clicked('qa-popup-ad-save')) {
-            qa_opt('qa-popup-ad-html', qa_post_text('qa-popup-ad-html'));
-            $ok = qa_lang('admin/options_saved');
-        }
+	public function admin_form(&$qa_content)
+	{
+		// process the admin form if admin hit Save-Changes-button
+		$ok = null;
+		if (qa_clicked('qa-popup-ad-save')) {
+			qa_opt('qa_popup_ad_only_first_page', (bool)qa_post_text('qa_popup_ad_only_first_page'));
+			qa_opt('qa_popup_ad_show_logged_in', (bool)qa_post_text('qa_popup_ad_show_logged_in'));
+			qa_opt('qa_popup_ad_scroll_percentage', qa_post_text('qa_popup_ad_scroll_percentage'));
+			qa_opt('qa_popup_ad_box_width', qa_post_text('qa_popup_ad_box_width'));
+			qa_opt('qa_popup_ad_box_height', qa_post_text('qa_popup_ad_box_height'));
+			qa_opt('qa_popup_ad_html_1', qa_post_text('qa_popup_ad_html_1'));
+			qa_opt('qa_popup_ad_html_2', qa_post_text('qa_popup_ad_html_2'));
+			qa_opt('qa_popup_ad_html_3', qa_post_text('qa_popup_ad_html_3'));
+			qa_opt('qa_popup_ad_html_4', qa_post_text('qa_popup_ad_html_4'));
+			qa_opt('qa_popup_ad_exclude_pages', qa_post_text('qa_popup_ad_exclude_pages'));
+			// qa_opt('qa-popup-ad-html', qa_post_text('qa-popup-ad-html'));
+			$ok = qa_lang('admin/options_saved');
+		}
 
-        // form fields to display frontend for admin
-        $fields = array();
+		// form fields to display frontend for admin
+		$fields = array();
 
-        $fields[] = array(
-            'type' => 'textarea',
-            'label' => 'html',
-            'tags' => 'name="qa-popup-ad-html"',
-            'value' => qa_opt('qa-popup-ad-html'),
-        );
+		$fields[] = array(
+			'label' => 'Show only the first page',
+			'type' => 'checkbox',
+			'value' => qa_opt('qa_popup_ad_only_first_page'),
+			'tags' => 'name="qa_popup_ad_only_first_page"',
+		);
 
-        return array(
-            'ok' => ($ok && !isset($error)) ? $ok : null,
-            'fields' => $fields,
-            'buttons' => array(
-                array(
-                    'label' => qa_lang_html('main/save_button'),
-                    'tags' => 'name="qa-popup-ad-save"',
-                ),
-            ),
-        );
-    }
+		$fields[] = array(
+			'label' => 'Show popup the user has logged in',
+			'type' => 'checkbox',
+			'value' => qa_opt('qa_popup_ad_show_logged_in'),
+			'tags' => 'name="qa_popup_ad_show_logged_in"',
+		);
+
+		$fields[] = array(
+			'label' => 'Scroll:',
+			'type' => 'number',
+			'suffix' => '%(1-100)',
+			'value' => qa_opt('qa_popup_ad_scroll_percentage'),
+			'tags' => 'name="qa_popup_ad_scroll_percentage"',
+		);
+
+		$fields[] = array(
+			'label' => 'Popup Box Width:',
+			'type' => 'number',
+			'value' => (int)qa_opt('qa_popup_ad_box_width'),
+			'suffix' => 'pixels',
+			'tags' => 'name="qa_popup_ad_box_width"',
+		);
+
+		$fields[] = array(
+			'label' => 'Popup Box Height:',
+			'type' => 'number',
+			'value' => (int)qa_opt('qa_popup_ad_box_height'),
+			'suffix' => 'pixels',
+			'tags' => 'name="qa_popup_ad_box_height"',
+		);
+
+		$fields[] = array(
+			'label' => 'Content HTML 1:',
+			'type' => 'textarea',
+			'value' => qa_opt('qa_popup_ad_html_1'),
+			'tags' => 'name="qa_popup_ad_html_1"',
+			'rows' => 2,
+		);
+		$fields[] = array(
+			'label' => 'Content HTML 2:',
+			'type' => 'textarea',
+			'value' => qa_opt('qa_popup_ad_html_2'),
+			'tags' => 'name="qa_popup_ad_html_2"',
+			'rows' => 2,
+		);
+		$fields[] = array(
+			'label' => 'Content HTML 3:',
+			'type' => 'textarea',
+			'value' => qa_opt('qa_popup_ad_html_3'),
+			'tags' => 'name="qa_popup_ad_html_3"',
+			'rows' => 2,
+		);
+		$fields[] = array(
+			'label' => 'Content HTML 4:',
+			'type' => 'textarea',
+			'value' => qa_opt('qa_popup_ad_html_4'),
+			'tags' => 'name="qa_popup_ad_html_4"',
+			'rows' => 2,
+		);
+
+		$fields[] = array(
+			'label' => 'Exclude Pages(separated by a line break):',
+			'type' => 'textarea',
+			'value' => qa_opt('qa_popup_ad_exclude_pages'),
+			'tags' => 'name="qa_popup_ad_exclude_pages"',
+			'rows' => 4,
+			'cols' => 15,
+		);
+		// $fields[] = array(
+		// 	'type' => 'textarea',
+		// 	'label' => 'html',
+		// 	'tags' => 'name="qa-popup-ad-html"',
+		// 	'value' => qa_opt('qa-popup-ad-html'),
+		// );
+
+		return array(
+			'ok' => ($ok && !isset($error)) ? $ok : null,
+			'fields' => $fields,
+			'buttons' => array(
+				array(
+					'label' => qa_lang_html('main/save_button'),
+					'tags' => 'name="qa-popup-ad-save"',
+				),
+			),
+		);
+	}
 }
