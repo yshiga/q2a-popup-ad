28e5e299b84304ad47032c22110a0d24428e8817
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-06-02 15:41:22 +0900

    [add]japanese translation file

---
+++ qa-popup-ad-lang-ja.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+	Plugin Name: POPUP AD
+	Plugin URI:
+	Plugin Description: popup add
+	Plugin Version: 0.1
+	Plugin Date: 2015-09-11
+	Plugin Author:yshiga
+	Plugin Author URI:https://github.com/yshiga
+	Plugin License: GPLv2
+	Plugin Minimum Question2Answer Version: 1.7
+	Plugin Update Check URI:
+*/
+
+	return array(
+		// default
+		'show_only_first' => 'アクセスした最初の1ページのみ表示する',
+		'show_logged_in' => 'ログイン済ユーザーにも表示する',
+		'scroll' => '表示するスクロールの割合:',
+		'popup_width' => 'ポップアップの幅:',
+		'popup_height' => 'ポップアップの高さ:',
+		'content_html' => '表示内容 HTML ^1:',
+		'exclude_page' => '表示しないページ(改行区切りで入力):',
+		'pixels' => 'ピクセル',
+	);
+
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
